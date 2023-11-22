de37e68b65300c72163e2d3527dde72669685ce7
LiamGunning07 <liamgunning07@gmail.com>
2023-11-22 16:27:58 -0700

    "with .forEach method"

--- functions/callbacks-intro/findWaldo.js
+++ functions/callbacks-intro/findWaldo.js
@@ -1,4 +1,4 @@
-const findWaldo = function(names, found) {
+/*const findWaldo = function(names, found) {
   for (let i = 0; i < names.length; i++) {
     let name = names[i];
     if (name === "Waldo") {
@@ -10,5 +10,20 @@ const findWaldo = function(names, found) {
 const actionWhenFound = function(index) {
   console.log("Found him at index " + index + "!");
 };
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+*/
+
+const findWaldo = function(names, found) {
+  names.forEach((name, index) => {
+    if (name === "Waldo") {
+      found(index);   // pass the index as an argument to the callback
+    }
+  });
+}
+
+const actionWhenFound = function(index) {
+  console.log("Found him on index: " + index);
+}
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
