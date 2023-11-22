0aa3413d2c5301f361c3471520bfc1ff3692def8
LiamGunning07 <liamgunning07@gmail.com>
2023-11-22 13:40:38 -0700

    "New Files"

--- random.js
+++ random.js
@@ -1,3 +1,14 @@
-var favouriteMeal = "BREAKFAST";
-
-console.log(favouriteMeal.toLowerCase());
+var planetMoons = {
+  mercury: 0,
+  venus: 0,
+  earth: 1,
+  mars: 2,
+  jupiter: 67,
+  saturn: 62,
+  uranus: 27,
+  neptune: 14
+};
+for (var planet in planetMoons) {
+  var numberOfMoons = planetMoons[planet];
+  console.log("Planet: " + planet + ", # of Moons: "+ numberOfMoons);
+}
