1890909ac903aa14a9fd1d8c6d67d3a1c8659cb0
LiamGunning07 <liamgunning07@gmail.com>
2023-11-23 15:06:28 -0700

    Linted Map ex.

---
+++ functions/map-builtin/mapTest.js
@@ -0,0 +1,3 @@
+const lighthouses = ["Gibraltar Point", "Peggy's Point", "Cove Island", "Discovery Island", "Cape Scott", "Point Clark", "Kincardine"];
+const nameLength = lighthouses.map(lighthouse => lighthouse.length)
+console.log(nameLength);
