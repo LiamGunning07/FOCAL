c4a53178d9cf1417f5a62a857a148797247bb630
LiamGunning07 <liamgunning07@gmail.com>
2023-11-29 13:54:07 -0700

    "done"

---
+++ main.js
@@ -0,0 +1,5 @@
+const sayHelloTo = require('./moduleCheck'); // How to call other files into our MAIN file
+
+console.log('sayHelloTo: ', sayHelloTo);
+
+sayHelloTo('Bernie');
---
+++ moduleCheck.js
@@ -0,0 +1,4 @@
+const sayHelloTo = function(person) {
+  console.log(`Hello, ${person}`);
+}
+module.exports = sayHelloTo; // This is how you export the function you would like to use inside of your MAIN file
